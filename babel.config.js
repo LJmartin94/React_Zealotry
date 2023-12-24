a3344b053c6fb47c92d9fb89e1be011d07ef67f2
limartin <lindsayjamesmartin94@gmail.com>
2023-12-24 04:46:18 +0100

    Add babel module resolver for path shortening syntax.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '#Features': './sources/features',
+          '#UI': './sources/ui',
+        },
+      },
+    ],
+  ],
 };
