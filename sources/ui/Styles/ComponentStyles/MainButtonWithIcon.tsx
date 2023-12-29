caae7edd1e88d8e84b611dc0fdcee07847c41b42
limartin <lindsayjamesmartin94@gmail.com>
2023-12-29 12:45:21 +0100

    Simplify MainButtonWithIcon by using existing utils.

--- sources/ui/Styles/ComponentStyles/MainButtonWithIcon.tsx
+++ sources/ui/Styles/ComponentStyles/MainButtonWithIcon.tsx
@@ -1,9 +1,7 @@
-import { StyleSheet } from 'react-native';
-
 import { Fill } from '../Size/Fill';
 import { Subtitle } from '../Text/Subtitle';
 import { VariableOpacity } from '../Transparency/VariableOpacity';
-import { StyleComposer } from '../utils/StyleComposer';
+import { ComponentStyleSheet } from '../utils/ComponentStyleSheet';
 import { ViewTextImgStyle } from '../utils/VTI';
 
 //ICON TYPE CURRENTLY UNKNOWN
@@ -11,19 +9,9 @@ export const MainButtonWithIcon = (
 	themedView: ViewTextImgStyle,
 	_icon: unknown
 ): ViewTextImgStyle => {
-	const viewStyle = StyleComposer([
-		themedView.view,
-		Fill.view,
-		VariableOpacity(0.9).view,
-	]);
-	const textStyle = StyleComposer([themedView.text, Subtitle.text]);
-	const imageStyle = StyleComposer([themedView.image]);
-
-	const style = StyleSheet.create({
-		view: StyleSheet.flatten(viewStyle),
-		text: StyleSheet.flatten(textStyle),
-		image: StyleSheet.flatten(imageStyle),
-	});
-
-	return style;
+	return ComponentStyleSheet(
+		[themedView.view, Fill.view, VariableOpacity(0.9).view],
+		[themedView.text, Subtitle.text],
+		[themedView.image]
+	);
 };
