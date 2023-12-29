3bcab84284dc7eb157c76ae7186f7d4d913ce652
limartin <lindsayjamesmartin94@gmail.com>
2023-12-29 02:52:40 +0100

    Create ViewTextImage as an interface to enforce strong typing.

--- sources/ui/Styles/ComponentStyles/BasicRoundButton.tsx
+++ sources/ui/Styles/ComponentStyles/BasicRoundButton.tsx
@@ -2,31 +2,29 @@ import { StyleSheet } from 'react-native';
 
 import { ViewTextImgStyle } from '../utils/VTI';
 
-export const BasicRoundButton = (): ViewTextImgStyle => {
-	return StyleSheet.create({
-		view: {
-			borderWidth: 1,
-			alignItems: 'center',
-			justifyContent: 'center',
-			width: 100,
-			height: 100,
-			borderRadius: 50,
-		},
-		text: {
-			borderWidth: 1,
-			alignItems: 'center',
-			justifyContent: 'center',
-			width: 100,
-			height: 100,
-			borderRadius: 50,
-		},
-		image: {
-			borderWidth: 1,
-			alignItems: 'center',
-			justifyContent: 'center',
-			width: 100,
-			height: 100,
-			borderRadius: 50,
-		},
-	});
-};
+export const BasicRoundButton: ViewTextImgStyle = StyleSheet.create({
+	view: {
+		borderWidth: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+	},
+	text: {
+		borderWidth: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+	},
+	image: {
+		borderWidth: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: 100,
+		height: 100,
+		borderRadius: 50,
+	},
+});
